71ae6e4c8e9c19ebec1d712539eb28e484dfdf50
HuuThuan19406 <54461614+HuuThuan19406@users.noreply.github.com>
2021-12-17 00:58:48 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+async function prediction(name) {
+    let data = await fetch('https://ml.itbclub.com/predict', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name: name })
+    })
+        .then(response => response.json());
+
+    return {
+        female: data.score[0].toFixed(2),
+        male: data.score[1].toFixed(2)
+    };
+}
+
+function handlePredict(score, maleElement, femaleElement, maleLabel, femaleLabel) {
+    score
+        .then(response => {
+            maleElement.style.width = maleLabel = response.male * 100 + '%';
+            femaleElement.style.width = femaleLabel.innerText = response.female * 100 + '%';
+        });
+}
